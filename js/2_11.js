b88435e29e397fefe7fb46da59323bcaef22ee37
Юлия Кобылина <JuliKobilina@gmail.com>
2019-12-16 15:24:10 +0200

    add2_11js

--- js/2_11.js
+++ js/2_11.js
@@ -6,6 +6,22 @@
 //Если пользователь ввёл 10w, то мы выводим ему количество секунд за 10 недел.
 //сли пользователь ввёл 10m, то мы выводим ему количество секунд за 10 месяц.
 //Проверяем то, что в конце)
-var hour=prompt("Enter number /d/h/w/m");
-var last = hour.slice(-1);
+var houruser=prompt("Enter number /d/h/w/m");
+var last = houruser.slice(-1);
+var hour=houruser.split(last);
+console.log(hour[0]);
+hours_to_number=parseInt(hour[0]);
+var result;
+if (last=="h"){
+    result=hour[0]*60;
+}else if(last="d"){
+    result=hour[0]*60*24;
 
+}else if(last="w"){
+    result=hour[0]*60*24*7;
+
+}else if(last="m"){
+    result=hour[0]*60*24*31;
+
+}
+console.log(result+"sec");
